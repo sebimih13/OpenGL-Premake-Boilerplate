70eafec3bf496f98a089280f4d6705e3917f1f0d
Sebastian Mihalache <sebimih@gmail.com>
2024-10-01 22:34:46 +0300

    Add GLAD

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # OpenGL-Premake-Boilerplate
+
+This project is a starting point for any application using GLFW, GLAD and GLM.  
+It uses Premake5 to generate the build files for Visual Studio 2022.  
+
+
+## Setup
+1. Clone the repository
+```sh
+git clone --recursive https://github.com/sebimih13/OpenGL-Premake-Boilerplate.git
+```
+
+2. Change the current working directory
+```sh
+cd OpenGL-Premake-Boilerplate
+```
+
+3. Generate the project
+```sh
+generate_project.bat
+```
---
+++ premake5.lua
@@ -0,0 +1,147 @@
+workspace "ComputerGraphics"
+    architecture "x64"
+    startproject "ComputerGraphics"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+    -- Ouput directories for bin and intermediate files
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    -- Include directories relative to root folder (solution directory)
+    IncludeDir = {}
+    IncludeDir["glfw"] = "vendor/glfw/include"
+    IncludeDir["glad"] = "vendor/glad/include"
+    IncludeDir["glm"] = "vendor/glm"
+
+
+
+    project "glfw"
+        location "bin-int/project-files"
+        kind "StaticLib"
+        language "C"
+        
+        targetdir("bin/" .. outputdir .. "/%{prj.name}")
+        objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "vendor/glfw/include/GLFW/glfw3.h",
+            "vendor/glfw/include/GLFW/glfw3native.h",
+            "vendor/glfw/src/**.c",
+            "vendor/glfw/src/**.h"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+            staticruntime "On"
+
+            defines 
+            { 
+                "_GLFW_WIN32",
+                "_CRT_SECURE_NO_WARNINGS"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
+
+
+
+    project "glad"
+        location "bin-int/project-files"
+        kind "StaticLib"
+        language "C"
+        
+        targetdir("bin/" .. outputdir .. "/%{prj.name}")
+        objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "vendor/glad/src/glad.c"
+        }
+
+        includedirs
+        {
+            "%{IncludeDir.glad}"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+            staticruntime "On"
+
+            defines 
+            { 
+                "_GLAD_WIN32",
+                "_CRT_SECURE_NO_WARNINGS"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
+
+
+
+    project "Engine"
+        location "Engine"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+
+        targetdir("bin/" .. outputdir .. "/%{prj.name}")
+        objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/**.h",
+            "%{prj.name}/**.cpp",
+            "%{prj.name}/**.hpp",
+            "%{prj.name}/**.c",
+            "%{prj.name}/**.vert",
+            "%{prj.name}/**.frag"
+        }
+
+        includedirs
+        {
+            "%{IncludeDir.glad}",
+            "%{IncludeDir.glfw}",
+            "%{IncludeDir.glm}"
+        }
+
+        links
+        {
+            "glfw",
+            "glad"
+        }
+
+        filter "system:windows"
+            staticruntime "On"
+            systemversion "latest"
+
+        filter "configurations:Debug"
+            symbols "On"
+
+            defines
+            {
+                "DEBUG"
+            }
+
+        filter "configurations:Release"
+            optimize "On"
+
+            defines
+            {
+                "NDEBUG"
+            }
+
